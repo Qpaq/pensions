188e09cab3fcb5e53d9824b1eb8b8421d1189710
Lukasz Szajkowski <lukasz@logicallysoftware.co.uk>
2014-02-19 08:35:39 +0000

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,32 @@
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from main.models import Pensioner
+
+def home(request):
+    pensioner = Pensioner.objects.filter(surname="Smith")[0]
+    return render(request, 'index.html', {'pensioner': pensioner})
+
+def index(request):
+    list = Pensioner.objects.all().order_by('-surname')[:5] #  List 5 members
+    return render(request, 'list.html', {'list': list})
+
+def detail(request, reference):
+    pensioner = Pensioner.objects.filter(reference=reference)[0]
+    return render(request, 'detail.html', {'pensioner': pensioner})
+
+def edit(request, reference):
+    pensioner = Pensioner.objects.filter(reference=reference)[0]
+
+    if request.method == 'POST':
+        pensioner.title = request.POST['title']
+        pensioner.forename = request.POST['forename']
+        pensioner.surname = request.POST['surname']
+        pensioner.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('main.views.home'))
+
+    return render(request, 'detail_edit.html', {'pensioner': pensioner})
